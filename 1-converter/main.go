b4afeb8a06b139d09791af2bfa7ae8f80b954fe9
Eugene Batogov <e.batogov@cti.ru>
2025-07-18 23:28:06 +0300

    Исправление замечаний

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -7,7 +7,7 @@ import (
 func main() {
 	const usdToEur = 0.85
 	const usdToRub = 78.5
+	const eurToRub = usdToRub / usdToEur
 
-	eurTORub := fmt.Sprintf("%.2f", 1/usdToEur*usdToRub)
-	fmt.Println("Euro to Ruble: ", eurTORub)
+	fmt.Println("Euro to Ruble: ", eurToRub)
 }
